af2b6f74a39939acb05beb5602516f15b872208c
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-01 12:00:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 
 def fizzbuzz(int)
+<<<<<<< HEAD
   if (int % 3 == 0) && (int % 5 == 0)
     "FizzBuzz"
   elsif int % 5 == 0
@@ -8,5 +9,15 @@ def fizzbuzz(int)
     "Fizz"
   else
     puts int
+=======
+  if !((int % 3 == 0) || (int % 5 == 0))
+    int
+  elsif (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
+  elsif int % 5 == 0
+    "Buzz"
+  else int % 3 == 0
+    "Fizz"
+>>>>>>> b82a7fff91ff1b4eefa4a3ce953684e8a7d9b48e
   end
 end
